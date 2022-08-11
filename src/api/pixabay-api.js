e37665c625544794bd45bcddc412ee47a2d3ca7f
Mykhailo Tsynkevych <99801093+mykhailotsynkevych@users.noreply.github.com>
2022-08-11 11:54:12 +0300

    Fix

--- src/api/pixabay-api.js
+++ src/api/pixabay-api.js
@@ -3,21 +3,6 @@ import axios from 'axios';
 axios.defaults.baseURL = 'https://pixabay.com/api/';
 const API_KEY = '28586147-3ab4251b0e4522a1aabc38539';
 
-export const fetchPhotos = (page = 1) => {
-  return axios
-    .get('?', {
-      params: {
-        q: 'all',
-        page,
-        key: API_KEY,
-        image_type: 'photo',
-        orientation: 'horizontal',
-        per_page: 12,
-      },
-    })
-    .then(response => response.data);
-};
-
 export const fetchQueryPhotos = (query, page = 1) => {
   return axios
     .get('?', {
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,78 +1,58 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
-import { fetchPhotos, fetchQueryPhotos } from '../api/pixabay-api';
+import { fetchQueryPhotos } from '../api/pixabay-api';
 import ImageGallery from './ImageGallery/ImageGallery';
 import Searchbar from './Searchbar/Searchbar';
 import Button from './Button/Button';
 import Modal from './Modal/Modal';
 import { Bars } from 'react-loader-spinner';
 
-
 const App = () => {
-  const [query, setQuery] = useState("");
+  const [query, setQuery] = useState('all');
   const [fotos, setFotos] = useState([]);
   const [totalHits, setTotalHits] = useState(0);
   const [page, setPage] = useState(1);
   const [loading, setLoading] = useState(false);
   const [modalFoto, setModalFoto] = useState(null);
 
-    const handleFormSubmit = query => {
-      setQuery(query);
-      setPage(1)
+  const handleFormSubmit = query => {
+    setQuery(query);
+    setPage(1);
   };
 
   const updatePage = () => {
-    setPage((page) => page + 1);
+    setPage(page => page + 1);
   };
 
-  const  funcModalFoto = (modalFoto = null) => {
+  const funcModalFoto = (modalFoto = null) => {
     setModalFoto(modalFoto);
   };
 
   useEffect(() => {
-        if (!query) {
-      setLoading(true);
-    fetchPhotos(page)
+    setLoading(true);
+    fetchQueryPhotos(query, page)
       .then(({ hits, totalHits }) => {
-          setFotos((fotos) => (page === 1 ? hits : [...fotos, ...hits]));
-          page === 1 && setTotalHits(totalHits);
+        setFotos(fotos => (page === 1 ? hits : [...fotos, ...hits]));
+        page === 1 && setTotalHits(totalHits);
       })
-      .catch(err => {
-        console.log(err);
-      })
+      .catch(err => console.log(err))
       .finally(() => setLoading(false));
-    }
-
-    if (query) {   
-      const setSearchNews = () => {
-      setLoading(true);
-      fetchQueryPhotos(query, page)
-        .then(({ hits, totalHits }) => {
-          setFotos((fotos) => (page === 1 ? hits : [...fotos, ...hits]));
-          page === 1 && setTotalHits(totalHits);
-        })
-        .catch((err) => console.log(err))
-        .finally(() => setLoading(false));
-    };
-    setSearchNews();
-    }
-
   }, [page, query]);
 
   return (
-          <div>
-        <Searchbar onSubmit={handleFormSubmit} />
-        {fotos.length > 0 ? (
-          <ImageGallery fotos={fotos} setModalFoto={funcModalFoto}/>
-        ) : null}
-        {fotos.length > 0 && fotos.length < totalHits && (
-          <Button updatePage={updatePage} />
-        )}
-        {modalFoto && (
-          <Modal modalFoto={modalFoto} setModalFoto={funcModalFoto} />
-        )}
-        {loading && <Bars color="#00BFFF" height={80} width={80} />}
-      </div>
+    <div>
+      <Searchbar onSubmit={handleFormSubmit} />
+      {fotos.length > 0 ? (
+        <ImageGallery fotos={fotos} setModalFoto={funcModalFoto} />
+      ) : null}
+      {fotos.length > 0 && fotos.length < totalHits && (
+        <Button updatePage={updatePage} />
+      )}
+      {modalFoto && (
+        <Modal modalFoto={modalFoto} setModalFoto={funcModalFoto} />
+      )}
+      {loading && <Bars color="#00BFFF" height={80} width={80} />}
+    </div>
   );
 };
 
